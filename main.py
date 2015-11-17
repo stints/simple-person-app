bba77ac2919a737e108e7932454d1813d092fa31
John Stinson <john.towery.stinson@gmail.com>
2015-11-16 21:42:31 -0600

    new css and javascript

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import datetime
 import json
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, request
 from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
@@ -13,23 +14,34 @@ mysql.init_app(app)
 
 @app.route("/")
 def main():
-  return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/api/person", methods=['GET','POST'])
 def person():
-  conn = mysql.connect()
-  cursor = conn.cursor()
-  if request.method == 'POST':
-    return json.dumps(request.get_json())
-  else:
-    cursor.execute('select first_name, last_name, birthday, zipcode from person')
-    data = cursor.fetchall()
-    columns = [desc[0] for desc in cursor.description]
-    result = []
-    for d in data:
-      d = dict(zip(columns, d))
-      result.append(d)
-    return json.dumps(result, default=date_handler)
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    if request.method == 'POST':
+        try:
+            data = request.get_json()
+            sql = "insert into person (first_name, last_name, birthday, zipcode)" \
+                    "values (%s, %s, %s, %s)".format(data['first_name'].strip(),
+                                                    data['last_name'].strip(),
+                                                    data['birthday'].strip(),
+                                                    data['zipcode'].strip())
+            cursor.execute(sql)
+            return json.dumps({"result":"inserted"})
+        except:
+            return json.dumps({"result":"error"})
+
+    else:
+        cursor.execute('select first_name, last_name, birthday, zipcode from person')
+        data = cursor.fetchall()
+        columns = [desc[0] for desc in cursor.description]
+        result = []
+        for d in data:
+            d = dict(zip(columns, d))
+            result.append(d)
+        return json.dumps(result, default=date_handler)
 
 def date_handler(obj):
-  return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
