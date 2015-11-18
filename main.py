3222e648cde6635c65016e577ce2aacc7fdb93c1
Ubuntu <ubuntu@ip-172-31-18-29.ec2.internal>
2015-11-18 23:54:18 +0000

    more crud functionality

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     return render_template("index.html")
 
 @app.route("/api/person", methods=['GET','POST'])
-def person():
+def gp_person():
     conn = mysql.connect()
     cursor = conn.cursor()
     if request.method == 'POST':
@@ -34,18 +34,20 @@ def person():
             try:
                 conn.commit()
                 result = "inserted"
+                id = cursor.lastrowid
             except:
                 conn.rollback()
                 result = "error"
+                id = -1
 
             conn.close()
-            return json.dumps({"result":result})
+            return json.dumps({"result":result,"id":id})
         except Exception as e:
             conn.close()
             return json.dumps({"result":"error","error":str(e)})
 
     else:
-        cursor.execute('select first_name, last_name, birthday, zipcode from person')
+        cursor.execute('select id, first_name, last_name, birthday, zipcode from person')
         data = cursor.fetchall()
         columns = [desc[0] for desc in cursor.description]
         result = []
@@ -54,6 +56,51 @@ def person():
             result.append(d)
         conn.close()
         return json.dumps(result, default=date_handler)
+
+@app.route('/api/person/<int:pid>', methods=['PUT','DELETE'])
+def pd_person(pid):
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    if request.method == 'DELETE':
+        cursor.execute('delete from person where id = %s', str(pid))
+        try:
+            conn.commit()
+            result = "deleted"
+        except:
+            conn.rollback()
+            result = "error"
+
+        conn.close()
+    elif request.method == 'PUT':
+        try:
+            data = request.get_json()
+            cursor.execute("""update person \
+                            set first_name = %s, \
+                                last_name = %s, \
+                                birthday = %s, \
+                                zipcode = %s \
+                            where id = %s""",
+                            (data['first_name'].strip(),
+                            data['last_name'].strip(),
+                            data['birthday'].strip(),
+                            data['zipcode'].strip(),
+                            int(data['id'].strip()))
+                          )
+            try:
+                conn.commit()
+                result = "updated"
+            except:
+                conn.rollback()
+                result = "error"
+        except Exception as e:
+            conn.close()
+            result = "major error"
+            return json.dumps({'error':str(e)})
+
+            conn.close()
+
+    return json.dumps({'result':result})
+
 
 def date_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
